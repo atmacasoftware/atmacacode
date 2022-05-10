336bff67d32d6d78a52a346cdeae0a804b3deb97
atmacasoftware <94282227+atmacasoftware@users.noreply.github.com>
2022-05-10 15:14:47 +0300

    layout.html eklendi

--- atmacacode/urls.py
+++ atmacacode/urls.py
@@ -20,7 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
