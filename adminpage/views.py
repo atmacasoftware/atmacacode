c0f87d0432e86d6824886056f6dc52a6102be532
atmaca <atmacasoftware52@gmail.com>
2022-05-11 08:56:06 +0300

    admin giriş sayfası yapıldı

--- adminpage/views.py
+++ adminpage/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
 
 # Create your views here.
+from django.views import View
+
 
 def admin_main_page(request):
-    return render(request, "adminpage/mainpage.html")
+    return render(request, "adminpage/signin.html")
+
+
--- atmacacode/urls.py
+++ atmacacode/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
+from django.views.generic import RedirectView
 from django.views.static import serve
 from django.conf import settings
 
@@ -22,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainpage.urls')),
     path('yonetim-paneli/', include('adminpage.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/img/logo/small_logo.png')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
