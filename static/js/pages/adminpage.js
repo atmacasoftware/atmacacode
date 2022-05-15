5f11ec1f6a163027c6200179668d0feac593dbf1
atmaca <atmacasoftware52@gmail.com>
2022-05-15 08:37:08 +0300

    notlar bölümü eklendi

--- static/js/pages/adminpage.js
+++ static/js/pages/adminpage.js
@@ -37,36 +37,36 @@ darkmode_toggle.click(function (e) {
 
 //Responsive Dashboard
 
-$(window).resize(function () {
-    if ($(window).width() < 992) {
-        $(".sidebar").css({"transform": "translate3d(-100%,10px,10px)"});
-        $('.toggler').click(function (e) {
-            $('.sidebar').css({"transform": "translateZ(0)"});
-            $(".page-content").css({"margin-left": "0"});
-            e.stopPropagation();
-            $(window).click(function () {
-                $(".sidebar").css({"transform": "translate3d(-100%,10px,10px)"});
-            })
-        })
-    }else{
-        $(".sidebar").css({"transform": "translate3d(0,0,0)"});
-        $('.toggler').click(function (e) {
-            $(".sidebar").toggleClass("trans");
-            $('.page-content').toggleClass('ml-0')
-        })
-    }
-})
-
-$(window).bind('resize', function (){
-    if($(this).width() < 992){
-        $(".sidebar").css({"transform": "translate3d(-100%,10px,10px)"});
-        $('.page-content').css({"margin-left":"0"});
-    }else {
-        $(".sidebar").css({"transform": "translate3d(0,0,0)"});
-        $('.page-content').css({"margin-left":"0"});
-    }
-})
-
+//$(window).resize(function () {
+//    if ($(window).width() < 992) {
+//        $(".sidebar").css({"transform": "translate3d(-100%,10px,10px)"});
+//        $('.toggler').click(function (e) {
+//            $('.sidebar').css({"transform": "translateZ(0)"});
+//            $(".page-content").css({"margin-left": "0"});
+//            e.stopPropagation();
+//            $(window).click(function () {
+//                $(".sidebar").css({"transform": "translate3d(-100%,10px,10px)"});
+//            })
+//        })
+//    }else{
+//        $(".sidebar").css({"transform": "translate3d(0,0,0)"});
+//        $('.toggler').click(function (e) {
+//            $(".sidebar").toggleClass("trans");
+//            $('.page-content').toggleClass('ml-0')
+//        })
+//    }
+//})
+//
+//$(window).bind('resize', function (){
+//    if($(this).width() < 992){
+//        $(".sidebar").css({"transform": "translate3d(-100%,10px,10px)"});
+//        $('.page-content').css({"margin-left":"0"});
+//    }else {
+//        $(".sidebar").css({"transform": "translate3d(0,0,0)"});
+//        $('.page-content').css({"margin-left":"0"});
+//    }
+//})
+//
 
 
 
