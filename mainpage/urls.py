e56b0963bcbe980d3221bc5639f339662b9b8c5c
atmacasoftware <94282227+atmacasoftware@users.noreply.github.com>
2022-07-16 14:00:35 +0300

    düzenleme 01

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from mainpage.views import index,contact
+from mainpage.views import index,contact,details
 urlpatterns = [
     path('', index, name='mainpage'),
     path('iletisim', contact, name='contact'),
+    path('<slug:slug>', details, name='slider_details')
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-
 from blog.models import Blog
 from mainpage.forms import ContactForm, SubscribeForm
+from mainpage.models import MainSlider
 from products.models import Services
 from user_accounts.models import Account
 # Create your views here.
@@ -9,6 +9,7 @@ from user_accounts.models import Account
 def index(request):
     blog = None
     services = None
+    mainslider = None
     try:
         blog = Blog.objects.all()[:3]
     except:
@@ -17,12 +18,16 @@ def index(request):
         services = Services.objects.all()
     except:
         pass
+    try:
+        mainslider = MainSlider.objects.all()[:10]
+    except:
+        pass
     if request.method == 'POST':
         form = SubscribeForm(request.POST)
         if form.is_valid():
             form.save()
-            return render(request, "mainpage/mainpage.html", {'blog': blog, 'services': services})
-    return render(request, "mainpage/mainpage.html",{'blog':blog,'services':services})
+            return render(request, "mainpage/mainpage.html", {'blog': blog, 'services': services,'mainslider':mainslider})
+    return render(request, "mainpage/mainpage.html",{'blog':blog,'services':services,'mainslider':mainslider})
 
 
 def contact(request):
@@ -37,3 +42,11 @@ def contact(request):
     return render(request, "mainpage/contact.html")
 
 
+def details(request,slug):
+    mainslider = None
+
+    try:
+        mainslider = MainSlider.objects.get(slug=slug)
+    except:
+        pass
+    return render(request, "mainpage/slider_detail.html", {'mainslider': mainslider})
